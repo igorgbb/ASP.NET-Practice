6402a7beaa9aee7fcfcf5e9c1f39aa6d19309308
igorgbb <bernardon.college@gmail.com>
2025-01-06 19:41:03 -0700

    Removed gibberish

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -4,5 +4,3 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
- git config --global user.email "bernardon.college@gmail.com"
- git config --global user.name "igorgbb"
